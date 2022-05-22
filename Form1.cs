f706149d573a0196771990926d87f02857dda020
ErenoGit <gracjan.dziedzic99@wp.pl>
2022-05-22 23:20:45 +0200

    First implementation

---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Discord_Nick_Randomizer
+{
+    public partial class Form1 : Form
+    {
+        public static string _apiToken;
+
+        public static bool _enabled = false;
+        public static int countNicks = 0;
+        public static int secondsDelayCount = 0;
+
+        public static string _guildId;
+        public static int _secondsDelay;
+        public static string[] _nicks;
+        public static bool _sequence;
+
+        private static Random _rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                while(true)
+                {
+                    if(_enabled)
+                    {
+                        secondsDelayCount--;
+                        if(secondsDelayCount <= 0)
+                        {
+                            if(_sequence)
+                            {
+                                if (countNicks == _nicks.Count())
+                                    countNicks = 0;
+
+                                Program.SetNick(_guildId, _nicks[countNicks]);
+                                countNicks++;
+                            }
+                            else
+                            {
+                                var random = _rand.Next(0, _nicks.Count());
+                                Program.SetNick(_guildId, _nicks[random]);
+                            }
+                            secondsDelayCount = _secondsDelay;
+                        }
+                    }
+
+                    Thread.Sleep(1000);
+                }
+            }).Start();
+        }
+
+        private void btnApplyToken_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textToken.Text))
+            {
+                MessageBox.Show("API token field cannot be empty!");
+                return;
+            }
+
+            _apiToken = textToken.Text;
+
+            var discordServersList = Program.GetAllServers();
+
+            dataGridServers.Rows.Clear();
+            foreach (var discordServer in discordServersList.ToList())
+            {
+                dataGridServers.Rows.Add(discordServer.id, discordServer.name);
+                dataGridServers.Refresh();
+            }
+
+            if (dataGridServers.Rows.Count > 0)
+                btnStartStop.Enabled = true;
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (_enabled)
+            {
+                _enabled = false;
+                btnStartStop.Text = "Start!";
+                status.Text = "Status: Stopped";
+                ToggleUi(true);
+            }
+            else
+            {
+                if (dataGridServers.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("You have to select row from server list!");
+                    return;
+                }
+
+                DataGridViewRow selectedRow = dataGridServers.SelectedRows[0];
+
+                _secondsDelay = (int)Math.Floor(numericSecondsChange.Value);
+                
+                if (_secondsDelay <= 0)
+                {
+                    MessageBox.Show("Number of seconds must be greater than 0!");
+                    return;
+                }
+
+                secondsDelayCount = _secondsDelay;
+                _guildId = selectedRow.Cells[0].Value.ToString();
+                _nicks = textListOfNicks.Lines.Where(x => !String.IsNullOrEmpty(x) && x.Length < 32).ToArray();
+                _sequence = checkBoxSequence.Checked;
+                _enabled = true;
+                btnStartStop.Text = "Stop!";
+                status.Text = "Status: Started";
+                countNicks = 0;
+                ToggleUi(false);
+            }
+        }
+
+        private void ToggleUi(bool toggle)
+        {
+            textToken.Enabled = toggle;
+            btnApplyToken.Enabled = toggle;
+            dataGridServers.Enabled = toggle;
+            textListOfNicks.Enabled = toggle;
+            checkBoxSequence.Enabled = toggle;
+            numericSecondsChange.Enabled = toggle;
+        }
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections.Specialized;
+using System.Text;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Discord_Nick_Randomizer
+{
+    public class DiscordServer
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public ulong permissions { get; set; }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+
+        public static List<DiscordServer> GetAllServers()
+        {
+            var discordServersList = new List<DiscordServer>();
+
+            try
+            {
+                using (var wc = new WebClient())
+                {
+                    wc.Headers.Set("Authorization", Form1._apiToken);
+                    var response = wc.DownloadString("https://discordapp.com/api/users/@me/guilds");
+                    discordServersList = JsonConvert.DeserializeObject<List<DiscordServer>>(response);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("An error occurred while trying to get the server list: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            discordServersList = discordServersList.Where(x => Convert.ToBoolean(x.permissions & 67108864) == true).ToList();
+
+            return discordServersList;
+        }
+
+
+        public static async void SetNick(string guildId, string nick)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", Form1._apiToken);
+                    client.BaseAddress = new Uri("https://discord.com");
+
+                    var listItemPayload = new Dictionary<string, object>
+                    {
+                        {"nick", nick}
+                    };
+
+                    var requestContent = new StringContent(JsonConvert.SerializeObject(listItemPayload));
+
+                    requestContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+                    var response = await client.PatchAsync(new Uri($"https://discord.com/api/guilds/{guildId}/members/@me"), requestContent);
+                    var data = response.Content.ReadAsStringAsync().Result.ToString();
+
+                    Debug.WriteLine("zwrotka: "+data);
+
+                    if (response.IsSuccessStatusCode == false)
+                    {
+                        throw (new Exception("An error occurred while trying to set name: " + data));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (new Exception("An error occurred while trying to set name: " + ex.Message));
+            }
+        }
+    }
+
+
+    public static class HttpClientExtensions
+    {
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent iContent)
+        {
+            var method = new HttpMethod("PATCH");
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = iContent
+            };
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            try
+            {
+                response = await client.SendAsync(request);
+            }
+            catch (TaskCanceledException e)
+            {
+                throw (new Exception("An error occurred while trying to set name: " + e.Message));
+            }
+
+            return response;
+        }
+    }
+
+}
